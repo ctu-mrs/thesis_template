8464417f6b6b13e2c8bd8006f3581cabe67e66ae
Tomas Baca <klaxalk@gmail.com>
2022-01-03 17:58:39 +0100

    wip

--- .ci/build.sh
+++ .ci/build.sh
@@ -17,18 +17,4 @@ pdftoppm -jpeg dissertation.pdf dissertation_thumbnail.jpg
 montage *.jpg -mode Concatenate -tile 3x1 dissertation_montage.jpg
 convert dissertation_montage-0.jpg -resize 1280 -quality 80 dissertation_montage-0.jpg
 mv main.pdf output/dissertation_thesis_template.pdf
-mv montage.jpg output/dissertation_thumbnail.jpg
-
-# pdflatex -interaction=nonstopmode bachelor.tex
-# pdftoppm -jpeg bachelor.pdf bachelor_thumbnail.jpg
-# montage *.jpg -mode Concatenate -tile 3x1 bachelor_montage.jpg
-# convert montage.jpg -resize 1280 -quality 80 bachelor_montage-0.jpg
-# mv main.pdf output/bachelor_thesis_template.pdf
-# mv montage.jpg output/bachelor_thumbnail.jpg
-
-# pdflatex -interaction=nonstopmode master.tex
-# pdftoppm -jpeg master.pdf master_thumbnail.jpg
-# montage *.jpg -mode Concatenate -tile 3x1 master_montage.jpg
-# convert montage.jpg -resize 1280 -quality 80 master_montage-0.jpg
-# mv main.pdf output/master_thesis_template.pdf
-# mv montage.jpg output/master_thumbnail.jpg
+mv dissertation_montage-0.jpg output/dissertation_thumbnail.jpg
