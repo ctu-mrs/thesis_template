9d7212c0a64330a8ad9f18efd7f2ab6269b5e809
Tomas Baca <klaxalk@gmail.com>
2022-01-04 14:34:42 +0100

    fixing ci

--- .ci/build.sh
+++ .ci/build.sh
@@ -9,20 +9,21 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 REPO_ROOT=`dirname "$0"`
 REPO_ROOT=`( cd "$REPO_ROOT/.." && pwd )`
 
-sudo apt -y update
-sudo apt-get -y install texlive texlive-latex-extra texlive-lang-czechslovak texlive-science texlive-pstricks latexmk texmaker texlive-font-utils texlive-fonts-extra texlive-bibtex-extra biber okular pdf-presenter-console dvipng sketch
-sudo apt -y install poppler-utils imagemagick
+# sudo apt -y update
+# sudo apt-get -y install texlive texlive-latex-extra texlive-lang-czechslovak texlive-science texlive-pstricks latexmk texmaker texlive-font-utils texlive-fonts-extra texlive-bibtex-extra biber okular pdf-presenter-console dvipng sketch
+# sudo apt -y install poppler-utils imagemagick
 
 BACHELOR_DIR=$REPO_ROOT/bachelor_thesis
 MASTER_DIR=$REPO_ROOT/master_thesis
 PHD_DIR=$REPO_ROOT/phd_thesis
 
-[ -e $REPO_ROOT/output ] && rm -rf $REPO_ROOT/output
-[ -e $REPO_ROOT/master_output ] && rm -rf $REPO_ROOT/master_output
+MASTER_OUTPUT_NAME=master_output
 
-mkdir -p $REPO_ROOT/master_output/.fig
+[ -e $REPO_ROOT/$MASTER_OUTPUT_NAME ] && rm -rf $REPO_ROOT/$MASTER_OUTPUT_NAME
 
-MASTER_OUTPUT_DIR=$REPO_ROOT/master_output
+mkdir -p $REPO_ROOT/$MASTER_OUTPUT_NAME/.fig
+
+MASTER_OUTPUT_DIR=$REPO_ROOT/$MASTER_OUTPUT_NAME
 
 cd $BACHELOR_DIR
 rm -rf build
@@ -31,8 +32,8 @@ cd build
 pdftoppm -jpeg main.pdf bachelor_thumbnail.jpg
 montage *.jpg -mode Concatenate -tile 3x1 bachelor_montage.jpg
 convert bachelor_montage-0.jpg -resize 1280 -quality 80 bachelor_montage-0.jpg
-mv main.pdf $MASTER_OUTPUT/bachelor_thesis_template.pdf
-mv bachelor_montage-0.jpg $MASTER_OUTPUT/.fig/bachelor_thesis_thumbnail.jpg
+mv main.pdf $MASTER_OUTPUT_DIR/bachelor_thesis_template.pdf
+mv bachelor_montage-0.jpg $MASTER_OUTPUT_DIR/.fig/bachelor_thesis_thumbnail.jpg
 cd $BACHELOR_DIR
 rm -rf build
 
@@ -43,8 +44,8 @@ cd build
 pdftoppm -jpeg main.pdf master_thumbnail.jpg
 montage *.jpg -mode Concatenate -tile 3x1 master_montage.jpg
 convert master_montage-0.jpg -resize 1280 -quality 80 master_montage-0.jpg
-mv main.pdf $MASTER_OUTPUT/master_thesis_template.pdf
-mv master_montage-0.jpg $MASTER_OUTPUT/.fig/master_thesis_thumbnail.jpg
+mv main.pdf $MASTER_OUTPUT_DIR/master_thesis_template.pdf
+mv master_montage-0.jpg $MASTER_OUTPUT_DIR/.fig/master_thesis_thumbnail.jpg
 cd $MASTER_DIR
 rm -rf build
 
@@ -55,8 +56,8 @@ cd build
 pdftoppm -jpeg main.pdf phd_thumbnail.jpg
 montage *.jpg -mode Concatenate -tile 3x1 phd_montage.jpg
 convert phd_montage-0.jpg -resize 1280 -quality 80 phd_montage-0.jpg
-mv main.pdf $MASTER_OUTPUT/phd_thesis_template.pdf
-mv phd_montage-0.jpg $MASTER_OUTPUT/.fig/phd_thesis_thumbnail.jpg
+mv main.pdf $MASTER_OUTPUT_DIR/phd_thesis_template.pdf
+mv phd_montage-0.jpg $MASTER_OUTPUT_DIR/.fig/phd_thesis_thumbnail.jpg
 cd $PHD_DIR
 rm -rf build
 
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Bachelor / Master / Ph.D. Thesis template [![Build Status](https://github.com/ctu-mrs/thesis_template/workflows/CI/badge.svg)](https://github.com/ctu-mrs/thesis_template/actions)
 
-## Click the thumbnail to download the pdf
-
 ### Bachelor
 
 [![This should be a thumbnail](https://github.com/ctu-mrs/thesis_template/raw/master/.fig/bachelor_thesis_thumbnail.jpg)](https://github.com/ctu-mrs/thesis_template/raw/master/bachelor_thesis_template.pdf)
@@ -36,3 +34,11 @@ The print version is enabled by uncommenting the first line in the `main.tex` do
 ```latex
 \newcommand*{\printversion}{}%
 ```
+
+## Editing the templates
+
+For those who wish to edit the templates, please, **edit the files in the Devel branch**.
+The **devel** branch contains more general structure with symlinks, which is not ideal for students.
+The CI job on Github takes care of repacking everything nicely into what you see in the **master** brach.
+The job takes approx. 5 minutes to run after you push to **devel**.
+See the [.ci/build.sh](.ci/build.sh) for more details.
